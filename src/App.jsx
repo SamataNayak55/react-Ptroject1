af3cd25a517f2a84cdb48d0ed3edf10a40855f67
samata.nayak55 <samata.nayak55@gmail.com>
2025-07-18 09:41:02 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Sam from './sam'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <Sam />
+    <h1>hii hello</h1>
+   </>
+  )
+}
+
+export default App
+ 
---
+++ src/sam.jsx
@@ -0,0 +1,6 @@
+function Sam () {
+    return (
+        <h3> samm iss good </h3>
+    )
+}
+export default Sam
